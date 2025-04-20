5614a8a8722d2a882740bd790fd685c181f78e24
aadeshd <adeshdalvi41@gmail.com>
2025-04-20 12:11:25 +0530

    Add files via upload
    Icebreaker code with Langchain - Gets LinkedIn URL using agent and tools and LinkedIn info of the Person.

---
+++ IceBreaker.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from linkedin import get_linkedin_info
+# from langchain.chat_models import ollama
+from linkedin_lookup_agent import lookup
+
+
+def get_info_of(name: str):
+    linkedin_url = lookup(name)
+    information = get_linkedin_info(linkedin_url)
+    return information
+
+
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+
+    information = get_info_of("Aadesh Dalvi IBM")
+
+    summary_template = """
+    Given the linkedin information {information} about a person I want create:
+    1. Short Summary
+    2. Two intresting points    
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    
+    # llm = ollama(model="llama3")
+
+    chain = summary_prompt_template | llm
+    output = chain.invoke(input={"information":information})
+
+    print("Response:")
+    print(output.to_json()['kwargs']['content'])
