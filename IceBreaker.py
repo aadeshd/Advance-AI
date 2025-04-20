684f8dafb89f07c97fe8a97098b149ddf3d59160
aadeshd <adeshdalvi41@gmail.com>
2025-04-20 12:15:30 +0530

    Update IceBreaker.py
    Code Refactored

--- IceBreaker.py
+++ IceBreaker.py
@@ -7,9 +7,26 @@ from linkedin_lookup_agent import lookup
 
 
 def get_info_of(name: str):
+
     linkedin_url = lookup(name)
     information = get_linkedin_info(linkedin_url)
-    return information
+
+    summary_template = """
+    Given the linkedin information {information} about a person I want create:
+    1. Short Summary
+    2. Two intresting points    
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    
+    chain = summary_prompt_template | llm
+    output = chain.invoke(input={"information":information})
+
+    print("Response:")
+    return (output.to_json()['kwargs']['content'])
 
 
 
@@ -18,23 +35,6 @@ if __name__ == "__main__":
     load_dotenv()
 
     information = get_info_of("Aadesh Dalvi IBM")
+    print(information)
 
-    summary_template = """
-    Given the linkedin information {information} about a person I want create:
-    1. Short Summary
-    2. Two intresting points    
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
     
-    # llm = ollama(model="llama3")
-
-    chain = summary_prompt_template | llm
-    output = chain.invoke(input={"information":information})
-
-    print("Response:")
-    print(output.to_json()['kwargs']['content'])
