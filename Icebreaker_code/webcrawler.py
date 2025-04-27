5d9977f408a2b6c19a161ea57f7d401399c33db1
aadeshd <adeshdalvi41@gmail.com>
2025-04-27 11:33:28 +0530

    Delete Icebreaker_code directory

--- Icebreaker_code/webcrawler.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-from dotenv import load_dotenv
-
-def get_linkedin_profile_tavily(txt: str):
-    load_dotenv()
-    # Search for Linkedin or Twitter profile page
-    search = TavilySearchResults()
-    res = search.run(f"{txt}")
-    return res
