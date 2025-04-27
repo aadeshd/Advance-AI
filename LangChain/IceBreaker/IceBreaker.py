a18a69fa025b45fe0c5a15b9f998564d233a661e
aadeshd <adeshdalvi41@gmail.com>
2025-04-27 11:34:31 +0530

    Add files via upload

---
+++ LangChain/IceBreaker/IceBreaker.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from linkedin import get_linkedin_info
+# from langchain.chat_models import ollama
+from linkedin_lookup_agent import lookup
+from outputParsers import summary_parser
+
+
+def get_info_of(name: str):
+
+    linkedin_url = lookup(name)
+    information = get_linkedin_info(linkedin_url)
+
+    summary_template = """
+    Given the linkedin information {information} about a person I want create:
+    1. Short Summary
+    2. Two intresting points
+    \n{format_instruction}    
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+        # ,partial_variables={"format_instruction": summary_parser.get_format_instructions()}
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    
+    # chain = summary_prompt_template | llm | summary_parser
+    chain = summary_prompt_template | llm 
+    output = chain.invoke(input={"information":information})
+
+    print("Response:")
+    return (output.to_json()['kwargs']['content'])
+
+
+
+if __name__ == "__main__":
+    
+    load_dotenv()
+
+    information = get_info_of("Aadesh Dalvi IBM")
+    print(information)
+
+    
